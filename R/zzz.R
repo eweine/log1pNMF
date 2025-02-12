cd4e8328791020055bce1c3fb4f00fd85b4bccfd
Eric Weine <ericw456@mit.edu>
2025-02-12 17:16:36 -0500

    added more openmp instructions

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,8 @@
   if (!openmp_available()) {
     packageStartupMessage(
       "Note: This package was compiled without OpenMP support.
-      For faster performance, please download OpenMP and reinstall the package."
+      For faster performance, please download OpenMP and reinstall the package.
+      If using a mac, follow the instructions here: https://mac.r-project.org/openmp/"
       )
   }
 }
