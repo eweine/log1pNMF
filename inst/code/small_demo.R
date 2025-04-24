9ce6ed913036fc8ac43ecbb557acd85081befb6a
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-24 15:32:38 -0500

    More improvements to the small demo.

--- inst/code/small_demo.R
+++ inst/code/small_demo.R
@@ -1,6 +1,6 @@
 # Short script to illustrate the use of passPCA on a toy data set.
 library(passPCA)
-set.seed(1)
+set.seed(3)
 
 # Simulate a data set from a Poisson NMF model with the shifted log
 # link.
@@ -9,10 +9,10 @@ m <- 400
 k <- 4
 L <- matrix(0,n,k)
 F <- matrix(0,m,k)
-shift_factor <- 0.5
+shift_factor <- 2
 for (j in 1:k) {
-  i <- sample(m,40)
-  F[i,j] <- abs(rnorm(40))
+  i <- sample(m,100)
+  F[i,j] <- abs(rnorm(100))
 }
 for (i in seq(1,n/2)) {
   j <- sample(k,1)
@@ -20,8 +20,13 @@ for (i in seq(1,n/2)) {
 }
 for (i in seq(n/2+1,n)) {
   j <- sample(k,2)
-  L[i,j] <- c(0.25,0.75)
+  x <- runif(1)
+  L[i,j] <- c(x,1 - x)
 }
+L <- cbind(runif(n),L)
+F <- cbind(abs(rnorm(m)),F)
+i <- sample(m,40)
+F[i,1] <- abs(10*rnorm(40))
 B <- tcrossprod(L,F)
 ginv <- function (x, s = 1)
   s * exp(x/max(1,s) - 1)
@@ -29,9 +34,16 @@ X <- rpois(n*m,ginv(B,s = shift_factor))
 X <- matrix(X,n,m)
 storage.mode(X) <- "double"
 
+# Function to compute the log-likelihood for a given Poisson log1p NMF
+# model.
+compute_loglik <- function (fit, X) {
+  return(sum(dpois(X,fitted(fit)),log = TRUE))
+}
+
 # Fit a Poisson NMF model with the shifted log link.
+k   <- k + 1
 fit <- fit_poisson_log1p_nmf(X,k,s = rep(1,n),cc = shift_factor,
-                             loglik = "exact",
+                             init_LL = L,init_FF = F,loglik = "exact",
                              control = list(maxiter = 500,threads = 1))
 
 # Compare the estimates to the truth.
@@ -45,28 +57,33 @@ L_est <- L_est %*% diag(1/s)
 F_est <- F_est %*% diag(s)
 print(cor(L,L_est))
 print(cor(F,F_est))
-plot(F,F_est,pch = 20,xlab = "truth",ylab = "estimate")
-abline(a = 0,b = 1,col = "magenta",lty = "dotted")
+# plot(F,F_est,pch = 20,xlab = "truth",ylab = "estimate")
+# abline(a = 0,b = 1,col = "magenta",lty = "dotted")
 
 # Now fit a Poisson NMF model with the shifted log like for different
 # values of the shift factor.
-shift_factors <- 10^seq(-1,1,length.out = 20)
-fits <- vector("list",length(shift_factors))
-L_mse <- rep(0,length(shift_factors))
-F_mse <- rep(0,length(shift_factors))
-names(fits)  <- as.character(shift_factors)
-names(L_mse) <- as.character(shift_factors)
-names(F_mse) <- as.character(shift_factors)
+shift_factors <- round(10^seq(-1.3,1,length.out = 20),digits = 4)
+fits   <- vector("list",length(shift_factors))
+loglik <- rep(0,length(shift_factors))
+L_mse  <- rep(0,length(shift_factors))
+F_mse  <- rep(0,length(shift_factors))
+names(fits)   <- as.character(shift_factors)
+names(loglik) <- as.character(shift_factors)
+names(L_mse)  <- as.character(shift_factors)
+names(F_mse)  <- as.character(shift_factors)
 for (shift_factor in shift_factors) {
   cat("shift_factor =",shift_factor,"\n")
 
   # Fit the model.
   fit <- fit_poisson_log1p_nmf(X,k,s = rep(1,n),cc = shift_factor,
-                               loglik = "exact",
+                               init_LL = L,init_FF = F,loglik = "exact",
                                control = list(maxiter = 200,threads = 1,
                                               verbose = FALSE))
   fits[[as.character(shift_factor)]] <- fit
 
+  # Compute the log-likelihood:
+  loglik[as.character(shift_factor)] <- compute_loglik(fit,X)
+  
   # Compare the estimates to the truth.
   L_est <- fit$LL
   F_est <- fit$FF
@@ -79,12 +96,25 @@ for (shift_factor in shift_factors) {
   L_mse[as.character(shift_factor)] <- mean((L_est - L)^2)
   F_mse[as.character(shift_factor)] <- mean((F_est - F)^2)
 }
+
+# Plot shift factor vs. MSE for L.
+par(mfcol = c(1,3))
 plot(shift_factors,L_mse,pch = 20,log = "x",col = "tomato",
-     xlab = "shift factor",ylab = "MSE",ylim = c(0,0.3))
+     xlab = "shift factor",ylab = "MSE(L)")
 lines(shift_factors,L_mse,col = "tomato")
 i <- which.min(L_mse)
 points(shift_factors[i],L_mse[i],pch = 1,cex = 1.2,col = "black")
-points(shift_factors,F_mse,col = "royalblue",pch = 20)
+
+# Plot shift factor vs. MSE for F.
+plot(shift_factors,F_mse,col = "royalblue",pch = 20,log = "x",
+     xlab = "shift factor",ylab = "MSE(F)")
 lines(shift_factors,F_mse,col = "royalblue")
 i <- which.min(F_mse)
 points(shift_factors[i],F_mse[i],pch = 1,cex = 1.2,col = "black")
+
+# Plot shift factor vs. log-likelihood.
+plot(shift_factors,loglik,pch = 20,log = "x",col = "limegreen",
+     xlab = "shift factor")
+lines(shift_factors,loglik,col = "limegreen")
+i <- which.max(loglik)
+points(shift_factors[i],loglik[i],pch = 1,cex = 1.2,col = "black")
