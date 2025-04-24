d36f44f67e3379040d49ab446b1c6a93e9f24871
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-24 11:23:22 -0500

    Added a comparison of fits for different settings of the shift factor to small_demo.R.

--- inst/code/small_demo.R
+++ inst/code/small_demo.R
@@ -47,3 +47,40 @@ print(cor(L,L_est))
 print(cor(F,F_est))
 plot(F,F_est,pch = 20,xlab = "truth",ylab = "estimate")
 abline(a = 0,b = 1,col = "magenta",lty = "dotted")
+
+# Now fit a Poisson NMF model with the shifted log like for different
+# values of the shift factor.
+shift_factors <- 10^seq(-1,1,length.out = 20)
+fits <- vector("list",length(shift_factors))
+L_mse <- rep(0,length(shift_factors))
+F_mse <- rep(0,length(shift_factors))
+names(fits)  <- as.character(shift_factors)
+names(L_mse) <- as.character(shift_factors)
+names(F_mse) <- as.character(shift_factors)
+for (shift_factor in shift_factors) {
+  cat("shift_factor =",shift_factor,"\n")
+
+  # Fit the model.
+  fit <- fit_poisson_log1p_nmf(X,k,s = rep(1,n),cc = shift_factor,
+                               loglik = "exact",
+                               control = list(maxiter = 200,threads = 1,
+                                              verbose = FALSE))
+  fits[[as.character(shift_factor)]] <- fit
+
+  # Compare the estimates to the truth.
+  L_est <- fit$LL
+  F_est <- fit$FF
+  kset <- apply(cor(L_est,L),2,which.max)
+  L_est <- L_est[,kset]
+  F_est <- F_est[,kset]
+  s     <- apply(L_est,2,max)
+  L_est <- L_est %*% diag(1/s)
+  F_est <- F_est %*% diag(s)
+  L_mse[as.character(shift_factor)] <- mean((L_est - L)^2)
+  F_mse[as.character(shift_factor)] <- mean((F_est - F)^2)
+}
+plot(shift_factors,L_mse,pch = 20,log = "x",col = "tomato",
+     xlab = "shift factor",ylab = "MSE",ylim = range(c(L_mse,F_mse)))
+lines(shift_factors,L_mse,col = "tomato")
+points(shift_factors,F_mse,col = "royalblue",pch = 20)
+lines(shift_factors,F_mse,col = "royalblue")
