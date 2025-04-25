f1ad0de715c4c7823ef840a06063a92e9278235f
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-25 16:20:40 -0500

    Made a few improvements to the plots in small_demo.R.

--- inst/code/small_demo.R
+++ inst/code/small_demo.R
@@ -42,7 +42,7 @@ compute_loglik <- function (fit, X) {
 
 # Fit a Poisson NMF model with the shifted log link.
 k   <- k + 1
-fit <- fit_poisson_log1p_nmf(X,k,s = rep(1,n),cc = shift_factor,
+fit <- fit_poisson_log1p_nmf(X,k,s = rep(1,n),cc = shift_factor_true,
                              init_LL = L,init_FF = F,loglik = "exact",
                              control = list(maxiter = 500,threads = 1))
 
@@ -65,12 +65,12 @@ print(cor(F,F_est))
 shift_factors <- round(10^seq(-1.3,1,length.out = 20),digits = 4)
 fits   <- vector("list",length(shift_factors))
 loglik <- rep(0,length(shift_factors))
-L_mse  <- rep(0,length(shift_factors))
-F_mse  <- rep(0,length(shift_factors))
+L_cor  <- rep(0,length(shift_factors))
+F_cor  <- rep(0,length(shift_factors))
 names(fits)   <- as.character(shift_factors)
 names(loglik) <- as.character(shift_factors)
-names(L_mse)  <- as.character(shift_factors)
-names(F_mse)  <- as.character(shift_factors)
+names(L_cor)  <- as.character(shift_factors)
+names(F_cor)  <- as.character(shift_factors)
 for (shift_factor in shift_factors) {
   cat("shift_factor =",shift_factor,"\n")
 
@@ -93,29 +93,43 @@ for (shift_factor in shift_factors) {
   s     <- apply(L_est,2,max)
   L_est <- L_est %*% diag(1/s)
   F_est <- F_est %*% diag(s)
-  L_mse[as.character(shift_factor)] <- mean((L_est - L)^2)
-  F_mse[as.character(shift_factor)] <- mean((F_est - F)^2)
+  # L_cor[as.character(shift_factor)] <- mean((L_est - L)^2)
+  # F_cor[as.character(shift_factor)] <- mean((F_est - F)^2)
+  L_cor[as.character(shift_factor)] <- mean(diag(cor(L_est,L)))
+  F_cor[as.character(shift_factor)] <- mean(diag(cor(F_est,F)))
 }
 
 # Plot shift factor vs. MSE for L.
 par(mfrow = c(2,3))
 i <- which.min(abs(shift_factor_true - shift_factors))
-plot(shift_factors,L_mse,pch = 20,log = "x",col = "darkorchid",
-     xlab = "shift factor",ylab = "MSE(L)")
-lines(shift_factors,L_mse,col = "darkorchid")
-points(shift_factors[i],L_mse[i],pch = 1,cex = 1.2,col = "black")
+plot(shift_factors,L_cor,pch = 20,log = "x",col = "darkorchid",
+     xlab = "shift factor",ylab = "mean correlation, L")
+lines(shift_factors,L_cor,col = "darkorchid")
+points(shift_factors[i],L_cor[i],pch = 1,cex = 1.2,col = "black")
 
 # Plot shift factor vs. MSE for F.
-plot(shift_factors,F_mse,col = "royalblue",pch = 20,log = "x",
-     xlab = "shift factor",ylab = "MSE(F)")
-lines(shift_factors,F_mse,col = "royalblue")
-points(shift_factors[i],F_mse[i],pch = 1,cex = 1.2,col = "black")
+plot(shift_factors,F_cor,col = "royalblue",pch = 20,log = "x",
+     xlab = "shift factor",ylab = "mean correlation, F")
+lines(shift_factors,F_cor,col = "royalblue")
+points(shift_factors[i],F_cor[i],pch = 1,cex = 1.2,col = "black")
 
 # Plot shift factor vs. log-likelihood.
 plot(shift_factors,loglik,pch = 20,log = "x",col = "dodgerblue",
      xlab = "shift factor")
 lines(shift_factors,loglik,col = "dodgerblue")
 points(shift_factors[i],loglik[i],pch = 1,cex = 1.2,col = "black")
+
+# Plot the condition number of F.
+compute_condition_number <- function (x) {
+  e <- eigen(cor(x))$values
+  n <- length(e)
+  return(e[1]/e[n])
+}
+cond_F <- sapply(fits,function (x) compute_condition_number(x$FF))
+plot(shift_factors,cond_F,pch = 20,log = "x",col = "olivedrab",
+     xlab = "shift factor",ylab = "orthogonality of F")
+lines(shift_factors,cond_F,col = "olivedrab")
+points(shift_factors[i],cond_F[i],pch = 1,cex = 1.2,col = "black")
 
 # Plot the sparsity of F.
 hoyer <- function (x) {
